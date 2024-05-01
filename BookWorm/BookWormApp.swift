4efa4d1c73263a1488ec2e38aae1cf4d581651a5
suryasai <suryasaigopalam@gmail.com>
2024-05-01 16:47:02 +0530

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by surya sai on 01/05/24.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
